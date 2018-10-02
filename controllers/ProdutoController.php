d57f24e4f6691ea78ffc58dcdc11230aa7808643
DiovaneMendes <diovane.m.mendes@gmail.com>
2018-10-02 19:34:12 -0300

    :computer: Começo back-end

--- controllers/ProdutoController.php
+++ controllers/ProdutoController.php
@@ -12,16 +12,41 @@
             return $response;
         }
 
-        public function buscarPorId(){
+        public function buscarPorId(Request $request, Response $response, array $args){
+            $id = $args['id'];
+    
+            $dao = new ProdutoDAO;    
+            $produto = $dao->buscarPorId($id);  
             
+            $response = $response->withJson($produto);
+            $response = $response->withHeader('Content-type', 'application/json');    
+            return $response;
         }
 
-        public function inserir(){
-            
+        public function inserir(Request $request, Response $response, array $args){
+            $var = $request->getParsedBody();
+            $produto = new Produto(0, $var['nome'], $var['preco']);
+    
+            $dao = new ProdutoDAO;    
+            $produto = $dao->inserir($produto);
+    
+            $response = $response->withJson($produto);
+            $response = $response->withHeader('Content-type', 'application/json');    
+            $response = $response->withStatus(201);
+            return $response;
         }
 
-        public function deletar(){
+        public function deletar(Request $request, Response $response, array $args){
+            $id = $args['id'];
             
+            $dao = new ProdutoDAO; 
+            $produto = $dao->buscarPorId($id);   
+           
+            $dao->deletar($id);
+    
+            $response = $response->withJson($produto);
+            $response = $response->withHeader('Content-type', 'application/json');    
+            return $response;
         }
     }
 ?>
--- index.php
+++ index.php
@@ -9,37 +9,16 @@
 
     $app = new \Slim\App;
 
-    $app->get('/produtos', function(Request $request, Response $response){
-        $dao = new ProdutoDAO;    
-        $array_produtos = $dao->listar();        
+    $app->get('/produtos', function(){
         
-        $response = $response->withJson($array_produtos);
-        $response = $response->withHeader('Content-type', 'application/json');    
-        return $response;
     });
 
-    $app->get('/produtos/{id}', function(Request $request, Response $response, array $args){
-        $id = $args['id'];
-
-        $dao = new ProdutoDAO;    
-        $produto = $dao->buscarPorId($id);  
+    $app->get('/produtos/{id}', function(){
         
-        $response = $response->withJson($produto);
-        $response = $response->withHeader('Content-type', 'application/json');    
-        return $response;
     });
 
-    $app->post('/produtos', function(Request $request, Response $response, array $args){
-        $var = $request->getParsedBody();
-        $produto = new Produto(0, $var['nome'], $var['preco']);
-
-        $dao = new ProdutoDAO;    
-        $produto = $dao->inserir($produto);
-
-        $response = $response->withJson($produto);
-        $response = $response->withHeader('Content-type', 'application/json');    
-        $response = $response->withStatus(201);
-        return $response;
+    $app->post('/produtos', function(){
+        
     });
 
     $app->put('/produtos/{id}', function(Request $request, Response $response, array $args){
@@ -55,16 +34,8 @@
         return $response;
     });
 
-    $app->delete('/produtos/{id}', function(Request $request, Response $response, array $args){
-        $id = $args['id'];
-
-        $dao = new ProdutoDAO; 
-        $produto = $dao->buscarPorId($id);   
-        $dao->deletar($id);
-
-        $response = $response->withJson($produto);
-        $response = $response->withHeader('Content-type', 'application/json');    
-        return $response;
+    $app->delete('/produtos/{id}', function(){
+        
     });
 
 
