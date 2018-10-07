e00d7b2f5e8033483533c0c93f66200c288a10c9
DiovaneMendes <diovane.m.mendes@gmail.com>
2018-10-07 04:07:22 -0300

    :game_die: Cria arquivo sql com criação de tabelas

--- Tabelas.sql
+++ Tabelas.sql
@@ -1,5 +1,39 @@
-CREATE TABLE PRODUTOS(
-    id SERIAL PRIMARY KEY,
+
+CREATE TABLE usuarios(
+    id_usuario SERIAL PRIMARY KEY,
+    senha VARCHAR(255)
+);
+
+CREATE TABLE autores(
+    id_autor PRIMARY KEY,
     nome VARCHAR(255),
-    preco FLOAT
+    pais VARCHAR(255)
+);
+
+CREATE TABLE livros(
+    id_livro SERIAL PRIMARY KEY,
+    isbn INTEGER,
+    nome VARCHAR(255),
+    autor INTEGER REFERENCES autores(id_autor),
+    editora VARCHAR(255),
+    ano DATE
+);
+
+CREATE TABLE clientes(
+    id_cliente SERIAL PRIMARY KEY,
+    matricula VARCHAR(255),
+    nome VARCHAR(255),
+    telefone VARCHAR(255)
+);
+
+CREATE TABLE retirada_livros(
+    id_retirada_livro SERIAL PRIMARY KEY,
+    quantidade_vezes INTEGER,
+    calculo_data_entrega DATE,
+    cliente INTEGER REFERENCES clientes(id_cliente)
+);
+
+CREATE TABLE devolucao_livros(
+    id_devolucao_livro SERIAL PRIMARY KEY,
+    verifica_atraso INTEGER
 );
