d4fd95efe70372a1909e1b38dffd8cbf52867e0c
diovanemendes <diovane.m.mendes@gmail.com>
2018-10-16 18:30:34 -0300

    :computer:

--- PDOFactory.php
+++ PDOFactory.php
@@ -2,20 +2,7 @@
     class PDOFactory{
         private static $pdo;
 
-        $db = parse_url(getenv("DATABASE_URL"));
-
-        $pdo = new PDO("pgsql:" . sprintf(
-            "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-            $db["host"],
-            $db["port"],
-            $db["user"],
-            $db["pass"],
-            ltrim($db["path"], "/")
-        ));
-
-
-        public static function getConexao()
-        {
+        public static function getConexao(){
             if(!isset($pdo)){
                 //$pdo = new PDO("mysql:host=localhost;dbname=biblioteca;charset=UTF8","root", "");
                 $pdo = new PDO("pgsql:host=localhost;dbname=biblioteca;user=postgres;password=postgres;");
