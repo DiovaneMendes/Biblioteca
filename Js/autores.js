e20b3c68d22d3e7543a266a41e4ab9726bd3eac7
diovanemendes <diovane.m.mendes@gmail.com>
2018-11-13 19:47:43 -0200

    :computer: Cria tabela de autores com jquery

---
+++ Js/autores.js
@@ -0,0 +1,45 @@
+$(document).ready(() => {
+    setInterval(loadJson,5000);    
+});
+
+let loadJson = () => {
+    $.get("/autores",
+        (jsonObject) => {
+            trataJson(jsonObject);
+        }
+    );
+}
+
+let trataJson = (jsonObject) => {
+    $("main").empty();
+    for(let indice in jsonObject){
+        
+        let table = $("<table></table>");
+        if(indice %3 == 1){
+            table.addClass("central");
+        }
+
+        let trCabecalho = $("<tr></tr>");
+        let thId = $("<th> Id </th>");
+        let thNome = $("<th> Nome </th>");
+        let thPais = $("<th> País </th>");
+        trCabecalho.append(thId, thNome, thPais);
+        table.append(trCabecalho);
+
+
+        let trCorpo = $("<tr></tr>");
+        let tdId = $("<td></td>");
+        let tdNome = $("<td></td>");
+        let tdPais = $("<td></td>");
+
+        tdId.append(jsonObject[indice].id_autor);
+        tdNome.append(jsonObject[indice].nome);
+        tdPais.append(jsonObject[indice].pais);
+
+        trCorpo.append(tdId, tdNome, tdPais);
+        table.append(trCorpo);
+
+        $("main").append(table);
+        console.log($("main").html());
+    }
+}
